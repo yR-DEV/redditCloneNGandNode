8401d1e6e2261981b24a71f7b572df8c39e474dc
Zack Claar <claar.zack@gmail.com>
2015-08-27 09:10:57 -0600

    Have new posts being updated in an array of objects, can't get ng-repeat working

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ var app = angular.module("LetsMakeReddit", []);
 app.controller("RedditClone", function($scope) {
   var posts = [];
   $scope.newPostBool = false;
+  $scope.posts = [{
+    author: "yR",
+    description: "Some reddit description post",
+    imageURL: "http://i.imgur.com/YezrFSe.jpg",
+    title: "some reddit post title"},
+  {
+    author: "Ry",
+    description: "post description reddit Some",
+    imageURL: "http://i.imgur.com/mEtORc7.jpg",
+    title: "title post reddit some"
+  }
+  ];
 
   $scope.newPostForm = function() {
     $scope.newPostBool = true;
@@ -10,10 +22,9 @@ app.controller("RedditClone", function($scope) {
 
   $scope.renderPost = function() {
     $scope.newPostBool = false;
-    var tempArr = [];
-    tempArr.push($scope.newPost);
-    $scope.posts.push(tempArr);
-    console.log($scope.posts);
+
+    $scope.posts.push($scope.newPost);
+    console.log($scope.posts[0].title);
   };
 
 });
